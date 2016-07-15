222f32b523128c19a0a587dfc22402338892104e
Han-Byul Kim <shine_star_11@Han-Byuls-MacBook-Pro.local>
2016-07-16 04:42:43 +0900

    drawer

--- app/src/main/java/app/com/example/hyunjunian/pokedic/MapsActivity.java
+++ app/src/main/java/app/com/example/hyunjunian/pokedic/MapsActivity.java
@@ -1,28 +1,119 @@
 package app.com.example.hyunjunian.pokedic;
 
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.view.View;
+import android.content.*;
+import android.widget.ListView;
+import android.widget.Toolbar;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import layout.info_window;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
+    private String[] navItems = {"Brown", "Cadet Blue", "Dark Olive Green",
+            "Dark Orange", "Golden Rod"};
+    private ListView lvNavList;
+    private FrameLayout flContainer;
+
+    private DrawerLayout dlDrawer;
+    private ActionBarDrawerToggle dtToggle;
+
+    public FrameLayout m_info_window_container;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        // Drawer
+        lvNavList = (ListView)findViewById(R.id.lv_activity_main_nav_list);
+        flContainer = (FrameLayout)findViewById(R.id.fl_activity_main_container);
+
+        lvNavList = (ListView)findViewById(R.id.lv_activity_main_nav_list);
+        flContainer = (FrameLayout)findViewById(R.id.fl_activity_main_container);
+
+        lvNavList.setAdapter(
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, navItems));
+        lvNavList.setOnItemClickListener(new DrawerItemClickListener());
+
+        dlDrawer = (DrawerLayout)findViewById(R.id.dl_activity_main_drawer);
+        dtToggle = new ActionBarDrawerToggle(this, dlDrawer, R.string.open_drawer, R.string.close_drawer){
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+        };
+        dlDrawer.setDrawerListener(dtToggle);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+        Button add = (Button)findViewById(R.id.add_pokemon);
+        //m_info_window_container = (FrameLayout) findViewById(R.id.info_window_up);
+
+        //int trans = android.support.v4.app.FragmentManager.beginTransaction();
+        //trans.add(m_info_window_container.id, new info_window(), "info_window");
+
+        add.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+                // 버튼 설치시 일어날 일
+            }
+        });
+
     }
+
+    protected void onPostCreate(Bundle savedInstanceState){
+        super.onPostCreate(savedInstanceState);
+        dtToggle.syncState();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(dtToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        dtToggle.onConfigurationChanged(newConfig);
+    }
+
 
 
     /**
@@ -43,7 +134,50 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        Marker sydneym = mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        LatLng MELBOURNE = new LatLng(-37.81319, 144.96298);
+        Marker melbourne = mMap.addMarker(new MarkerOptions()
+                .position(MELBOURNE)
+                .title("Melbourne")
+                .snippet("Population: 4,137,400"));
+
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener{
+
+        @Override
+        public void onItemClick(AdapterView<?> adapter, View view, int position,
+                                long id) {
+            switch(position){
+                case 0:
+                    flContainer.setBackgroundColor(Color.parseColor("#A52A2A"));
+                    break;
+                case 1:
+                    flContainer.setBackgroundColor(Color.parseColor("#5F9EA0"));
+                    break;
+                case 2:
+                    flContainer.setBackgroundColor(Color.parseColor("#556B2F"));
+                    break;
+                case 3:
+                    flContainer.setBackgroundColor(Color.parseColor("#FF8C00"));
+                    break;
+                case 4:
+                    flContainer.setBackgroundColor(Color.parseColor("#DAA520"));
+                    break;
+            }
+            dlDrawer.closeDrawer(lvNavList);
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 }
+
+
